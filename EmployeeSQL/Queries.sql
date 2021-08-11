a7119ec35094dbf0d4c43faaec1357e1c586fd6f
Benjamin Tran <Benjietran1994@gmail.com>
2021-08-11 15:46:57 -0700

    Formatting

--- EmployeeSQL/Queries.sql
+++ EmployeeSQL/Queries.sql
@@ -1,24 +1,37 @@
 -- List the following details of each employee: employee number, last name, first name, sex, and salary.
-Select employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+Select employees.emp_no
+	, employees.last_name
+	, employees.first_name
+	, employees.sex
+	, salaries.salary
 From employees
 Join salaries on employees.emp_no = salaries.emp_no;
 
 -- List first name, last name, and hire date for employees who were hired in 1986.
 -- We use LIKE because hire date contains dates and months as well so we want any date that contains any combination with the year 1986
-Select first_name, last_name, hire_date
+Select first_name
+	, last_name
+	, hire_date
 From employees
 Where hire_date LIKE '%1986';
 
 -- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 -- To do this, must join departments with dept_manager and employees
-Select dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+Select dept_manager.dept_no
+	, departments.dept_name
+	, dept_manager.emp_no
+	, employees.last_name
+	, employees.first_name
 From dept_manager
 Join departments on dept_manager.dept_no = departments.dept_no
 Join employees on dept_manager.emp_no = employees.emp_no;
 
 
 -- List the department of each employee with the following information: employee number, last name, first name, and department name.
-Select employees.emp_no,employees.last_name,employees.first_name,departments.dept_name
+Select employees.emp_no
+	,employees.last_name
+	,employees.first_name
+	,departments.dept_name
 From employees
 Join dept_emp on employees.emp_no = dept_emp.emp_no
 Join departments on dept_emp.dept_no = departments.dept_no;
@@ -27,7 +40,9 @@ Join departments on dept_emp.dept_no = departments.dept_no;
 -- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 -- Once again, because we are looking for ANY last names that begin with B, we can use LIKE along with a wildcard operator after B
 -- Also, this dataset MUST be made up, I refuse to believe any company that contains 20 employees with the name Hercules and even some are female...
-Select first_name,last_name,sex
+Select first_name
+	,last_name
+	,sex
 From employees
 Where first_name='Hercules' and last_name LIKE 'B%';
 
@@ -36,7 +51,10 @@ Where first_name='Hercules' and last_name LIKE 'B%';
 -- Need to find out the designated number of the Sales department
 select * from departments
 -- Sales department is d007
-Select employees.emp_no,employees.last_name,employees.first_name,departments.dept_name
+Select employees.emp_no
+	,employees.last_name
+	,employees.first_name
+	,departments.dept_name
 from employees
 join dept_emp on employees.emp_no=dept_emp.emp_no
 join departments on dept_emp.dept_no=departments.dept_no
@@ -47,7 +65,10 @@ where departments.dept_no='d007';
 -- Need to find out the designated number of the Development department
 select * from departments
 -- Development department is d005
-Select employees.emp_no,employees.last_name,employees.first_name,departments.dept_name
+Select employees.emp_no
+	,employees.last_name
+	,employees.first_name
+	,departments.dept_name
 from employees
 join dept_emp on employees.emp_no=dept_emp.emp_no
 join departments on dept_emp.dept_no=departments.dept_no
